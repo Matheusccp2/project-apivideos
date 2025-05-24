def7aec85eb4d985f92e373bc9d863a68fd1db43
Pedro Henrique <contato.matheusccp5@gmail.com>
2025-05-24 12:31:31 -0300

    feat: create POST

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 import { fastify } from "fastify";
+import { DatabaseMemory } from "./database-memory.js";
 
 const server = fastify();
+const database = new DatabaseMemory();
 
 server.post("/videos", (res, req) => {
   return "Hello World";
+server.post("/videos", (req, res) => {
+  const { title, description, duration } = req.body;
+
+  database.create({
+    title,
+    description,
+    duration,
+  });
+
+  return res.status(201).send();
 });
 
 server.get("/videos", (res, req) => {
